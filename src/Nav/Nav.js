169782a0c9a6cc24927fe028c16f2e1e597621f6
Trent Smith <trentnsmith@gmail.com>
2020-07-23 23:58:58 -0500

    working on hamburger menu functionality, not working yet

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Hamburger from './Hamburger/Hamburger';
 import './Nav.css';
 
 class Nav extends Component {
@@ -6,11 +8,36 @@ class Nav extends Component {
         return (
             <div className="Nav">
                 <h1>
+                    <Link to='/' className="h1_link">
                     White Lightning
+                    </Link>
                 </h1>
                 <p>
                     Explore Omaha's Distilleries
                 </p>
+                <ul className="menu">
+                    <li>
+                        <Link to='/' className="menu_link">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/distilleries' className="menu_link">
+                            Distilleries
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/all-spirits' className="menu_link">
+                            Spirits
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='add-spirits' className="menu_link">
+                            About
+                        </Link>
+                    </li>
+		        </ul>
+                <Hamburger />
             </div>
         );
     };
