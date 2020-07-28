ee65a38de236f4cd157a73fd3c96f6114d22cd84
Trent Smith <trentnsmith@gmail.com>
2020-07-28 18:30:57 -0500

    adding font awesome icons

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -6,28 +6,28 @@ class LandingPage extends Component {
     render () {
         return (
             <div className="landing_page">
+                <div className="background_container">
                 <div className="lp_container">
                     <Link to='/distilleries' className="box">
-                        (graphic)
+                    <i class="fas fa-warehouse"></i>
                         <div>
-                            Distilleries
                             Check out the Distilleries of Omaha
                         </div>
                     </Link>
                     <Link to='/all-spirits' className="box">
-                        (graphic)
+                    <i class="fas fa-wine-bottle"></i>
                         <div>
-                            Spirits
                             Learn about the local whiskeys
                         </div>
                     </Link>
                     <Link to='/about-page' className="box">
-                        (graphic)
+                    <i class="fas fa-question"></i>
                         <div>
                             Why White Lightning?
                             History of distilling and the connection to Omaha
                         </div>
                     </Link>
+                </div>
                 </div>
             </div>
         );
