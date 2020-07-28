c8fe4727aad1d13203d0d3d3ec43107c248d82f3
Trent Smith <trentnsmith@gmail.com>
2020-07-28 18:40:08 -0500

    creating validation component; rendering message is appearing red

---
+++ src/Validation/Validation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Validation.css';
+
+export default function ValidationError(props) {
+  if(props.message) {
+    return (
+      <div className="error">{props.message}</div>
+    );
+  }
+
+  return <></>
+}
