da4eb78bb33dc00d5ac228ce105b7949f1200e86
Trent Smith <trentnsmith@gmail.com>
2020-07-28 18:28:24 -0500

    adding validation and styling to form, validation working, api call working

--- src/AddSpirits/AddSpirits.js
+++ src/AddSpirits/AddSpirits.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './AddSpirits.css';
 import SpiritContext from '../SpiritContext';
 import { Link } from 'react-router-dom';
+import ValidationError from '../Validation/Validation';
 import config from '../config';
 
 class AddSpirits extends Component {
@@ -37,6 +38,7 @@ class AddSpirits extends Component {
     handleSpiritName = (e) => {
         let {spirit_name} = this.state
         spirit_name.value = e.target.value
+        spirit_name.touched = true
         this.setState({spirit_name})
     };
 
@@ -49,24 +51,28 @@ class AddSpirits extends Component {
     handleContent = (e) => {
         let {content} = this.state
         content.value = e.target.value
+        content.touched = true
         this.setState({content})
     };
 
     handleCategory = (e) => {
         let {category} = this.state
         category.value = e.target.value
+        category.touched = true
         this.setState({category})
     };
 
     handleAge = (e) => {
         let {age} = this.state
         age.value = e.target.value
+        age.touched = true
         this.setState({age})
     };
 
     handleAbv = (e) => {
         let {abv} = this.state
         abv.value = e.target.value
+        abv.touched = true
         this.setState({abv})
     };
 
@@ -85,12 +91,10 @@ class AddSpirits extends Component {
             return;
         }
         if (
-            this.state.spirit_name, 
-            this.state.content,
-            this.state.age,
-            this.state.abv,
-            this.state.category
-            // this.state.distillsId.value !== null
+            this.validateName() === true &&
+            this.validateCategory() === true &&
+            this.validateAge() === true &&
+            this.validateAbv() === true
             ) {
             fetch(`${config.API_ENDPOINT}/spirits`,
                 {   method: 'POST',
@@ -112,6 +116,61 @@ class AddSpirits extends Component {
             };
     };
 
+    validateName = () => {
+        let name = this.state.spirit_name.value.trim();
+        if (!this.state.spirit_name.touched) {
+            return false;
+        };
+        if (name.length < 3) {
+            return 'Must be min 3 digits';
+        };
+        return true;
+    };
+
+    validateCategory = () => {
+        let category = this.state.category.value.trim();
+        if (!this.state.category.touched) {
+            return false;
+        };
+        if (category.length < 3) {
+            return 'Must be min 3 digits';
+        };
+        return true;
+    };
+
+    validateAge = () => {
+        let age = this.state.age.value.trim();
+        if (!this.state.age.touched) {
+            return false;
+        };
+        if (age.length < 3) {
+            return 'Must be min 3 digits';
+        };
+        return true;
+    };
+
+    validateAbv = () => {
+        let abv = this.state.abv.value.trim();
+        if (!this.state.abv.touched) {
+            return false;
+        };
+        if (abv.length < 3) {
+            return 'Must be min 3 digits';
+        };
+        return true;
+    };
+
+    validateContent = () => {
+        let content = this.state.content.value.trim();
+        if (!this.state.content.touched) {
+            return false;
+        };
+        if (content.length < 20) {
+            return 'Must be min 20 digits';
+        };
+        return true;
+    };
+
     render () {
         return (
             <div className="add_spirits">
@@ -122,13 +181,14 @@ class AddSpirits extends Component {
                             <label className="name_label">
                                 Name of the Spirit:
                             </label>
-                            <input className="name_input" type="text" onChange={this.handleSpiritName} />
+                            <input className="name_input" type="text" onChange={this.handleSpiritName} required/>
+                            <ValidationError message={this.validateName()}/>
                         </div>
                         <div className="section">
                             <label className="distillery_choice">
                                 Distillery:
                             </label>
-                            <select onChange={this.handleSpiritDistillsId}>
+                            <select className="distil_select" onChange={this.handleSpiritDistillsId} required>
                                 <option value={null}>Choose Distillery</option>
                                 {this.context.distills.map(distil => 
                                     <option
@@ -143,22 +203,33 @@ class AddSpirits extends Component {
                             <label className="category_label">
                                 Type of Spirit:
                             </label>
-                            <input className="category_input" type="text" onChange={this.handleCategory} />
+                            <input className="category_input" type="text" onChange={this.handleCategory} required/>
+                            <ValidationError message={this.validateCategory()}/>
                         </div>
                         <div className="section">
                             <label className="age_label">
                                 Aged:
                             </label>
-                            <input className="age_input" type="text" onChange={this.handleAge} />
+                            <input className="age_input" type="text" onChange={this.handleAge} required/>
+                            <ValidationError message={this.validateAge()}/>
                         </div>
                         <div className="section">
                             <label className="abv_label">
                                 ABV/Proof:
                             </label>
-                            <input className="abv_input" type="text" onChange={this.handleAbv} />
+                            <input className="abv_input" type="text" onChange={this.handleAbv} required/>
+                            <ValidationError message={this.validateAbv()}/>
                         </div>
                         <div className="section">
-                            <textarea className="content" placeholder="Describe this spirit! Taste/Smell/Smoothness..." onChange={this.handleContent} />
+                            <textarea 
+                                className="content" 
+                                placeholder="Describe this spirit! Taste/Smell/Smoothness..." 
+                                onChange={this.handleContent}
+                                rows="5"
+                                cols="40"
+                                required
+                            />
+                            <ValidationError message={this.validateContent()}/>
                         </div>
                         <div className="submit_section">
                             <input className="form_submit" type="submit" value="Add Spirit" />
