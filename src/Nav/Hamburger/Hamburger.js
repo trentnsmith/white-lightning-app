a5fc8e77dec0ef748a63eb9b96378ab73f017fd8
Trent Smith <trentnsmith@gmail.com>
2020-07-28 18:32:33 -0500

    adding hamburger component, added dropdown with links for navigating app

--- src/Nav/Hamburger/Hamburger.js
+++ src/Nav/Hamburger/Hamburger.js
@@ -3,44 +3,72 @@ import { Link } from 'react-router-dom';
 import './Hamburger.css';
 
 class Hamburger extends Component {
-    
+    constructor() {
+        super();
+
+        this.state = {
+            showMenu: false,
+        };
+
+        this.showMenu = this.showMenu.bind(this);
+        this.closeMenu = this.closeMenu.bind(this);
+    };
+
+    showMenu(event) {
+        event.preventDefault();
+
+        this.setState({ showMenu: true }, () => {
+            document.addEventListener('click', this.closeMenu);
+          });
+    }
    
+    closeMenu() {
+        this.setState({ showMenu: false }, () => {
+            document.removeEventListener('click', this.closeMenu);
+        });
+    };
+
     render() {
         return (
             <div className="hamburger">
-                <button>
-                    <i class="fas fa-bars nav-toggle"></i>
+                <button class="ham_button">
+                    <i class="fas fa-bars" onClick={this.showMenu}></i>
                 </button>
-                <div className="main-navigation">
-                <i class="fas fa-times"></i>
-                <ul className="ham_menu">
-                    <li>
-                        <Link to="/" className="ham_link">
-                            Home
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='/Distilleries' className="ham_link">
-                            Distilleries
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='/all-spirits' className="ham_link">
-                            Spirits
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='add-spirits' className="ham_link">
-                            Add A New Drink
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='about' className="ham_link">
-                            About
-                        </Link>
-                    </li>
-                </ul>
-                </div>
+
+                {this.state.showMenu ? (
+                    <div className="main-navigation">
+                        <ul className="ham_menu">
+                            <li className="ham_menu_items">
+                                <Link to="/" className="ham_link">
+                                    Home
+                                </Link>
+                            </li>
+                            <li className="ham_menu_items">
+                                <Link to='/Distilleries' className="ham_link">
+                                    Distilleries
+                                </Link>
+                            </li>
+                            <li className="ham_menu_items">
+                                <Link to='/all-spirits' className="ham_link">
+                                    Spirits
+                                </Link>
+                            </li>
+                            <li className="ham_menu_items">
+                                <Link to='add-spirits' className="ham_link">
+                                    Add A New Drink
+                                </Link>
+                            </li>
+                            <li className="ham_menu_items">
+                                <Link to='about-page' className="ham_link">
+                                    About
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                ) :
+                    (null)
+                }
+
             </div>
         );
     };
